20057510904422cb9909b1a92e551a906001d6ba
Ronney <ronneyrv@gmail.com>
2026-06-22 11:54:59 -0300

    feat: implement custom user details service

---
+++ src/main/java/com/ronney/finance/security/CustomUserDetails.java
@@ -0,0 +1,29 @@
+package com.ronney.finance.security;
+
+import com.ronney.finance.domain.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return  user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
---
+++ src/main/java/com/ronney/finance/security/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.ronney.finance.security;
+
+public class SecurityConfig {
+}
