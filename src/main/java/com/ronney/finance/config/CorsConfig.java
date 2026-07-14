ff670d86b4b680520874933c0f892b88dbdd793a
Ronney <ronneyrv@gmail.com>
2026-07-14 10:54:17 -0300

    fix(cors): allow PATCH requests

--- src/main/java/com/ronney/finance/config/CorsConfig.java
+++ src/main/java/com/ronney/finance/config/CorsConfig.java
@@ -31,6 +31,7 @@ public class CorsConfig {
                         "GET",
                         "POST",
                         "PUT",
+                        "PATCH",
                         "DELETE",
                         "OPTIONS"
                 )
