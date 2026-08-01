cafc97ab5f206ad67255dac077d4a60005b38f83
Ronney <ronneyrv@gmail.com>
2026-08-01 18:45:07 -0300

    chore(test): use Flyway schema in integration tests

--- src/main/resources/db/migration/V13__create_refresh_tokens.sql
+++ src/main/resources/db/migration/V13__create_refresh_tokens.sql
@@ -1,8 +1,8 @@
 CREATE TABLE refresh_tokens (
     id UUID PRIMARY KEY,
     token VARCHAR(36) NOT NULL UNIQUE,
-    expires_at TIMESTAMPTZ NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
+    expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     revoked BOOLEAN NOT NULL,
     user_id UUID NOT NULL,
 
